5fdab4c37cdebff8293075347435b5aa8a0db0e9
Trond Fröding Hatlen <trondfroding@gmail.com>
2018-01-29 13:20:07 +0100

    Accept and log github webhook requests

---
+++ index.js
@@ -0,0 +1,52 @@
+const WebhooksApi = require('@octokit/webhooks');
+
+const webhooks = new WebhooksApi({
+  secret: process.env.GITHUB_SECRET,
+});
+
+webhooks.on('deployment', ({ id, name, payload }) => {
+  console.log(
+    'DEPLOYMENT:',
+    name,
+    'event received\n',
+    JSON.stringify({ id, name, payload }, null, 2),
+  );
+});
+
+webhooks.on('pull_request', ({ payload }) => {
+  console.log(
+    'PULL REQUEST event received:\n',
+    JSON.stringify({ payload }, null, 2),
+  );
+
+  const {
+    pull_request: { url, state },
+    statuses_url: statusesUrl, // probably used for setting the pull request status
+    head: {
+      ref, // branch-name
+      sha, // commit sha
+    },
+  } = payload;
+
+  console.log('data:');
+  console.log({
+    url,
+    state,
+    statusesUrl,
+    ref,
+    sha,
+  });
+});
+
+webhooks.on('push', ({ id, name, payload }) => {
+  console.log(
+    'PUSH:',
+    name,
+    'event received\n',
+    JSON.stringify({ id, name, payload }, null, 2),
+  );
+});
+
+require('http')
+  .createServer(webhooks.middleware)
+  .listen(process.env.PORT || 3000);
